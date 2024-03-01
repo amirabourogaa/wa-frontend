65b599a8da3f0b7ee70d9246d5d8f570788c51ac
amirabourogaa <abouragba@u-label.com>
2024-03-02 00:25:18 +0100

    correct cart grid css

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -7,10 +7,18 @@ import {
   deleteProduct,
 } from "../app/features/cart/cartSlice";
 import axios from "axios";
-import { Button } from "@mui/material";
+import { Button, Grid, Paper } from "@mui/material";
 import { DarkModeContext } from "../contexte";
 import Appbar from './../components/Appbar';
+import { experimentalStyled as styled } from '@mui/material/styles';
 
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(2),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 const Cart = () => {
   const { cartList } = useSelector((state) => state.cart);
   const [message, setMessage] = useState("");
@@ -62,9 +70,12 @@ const Cart = () => {
     >
       
       <Container>
-        <Row className="justify-content-center">
-          <Col md={8}>
-            {cartList.length === 0 && (
+
+      <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+
+    <Grid item xs={2} sm={4} md={12} >
+      <Item>
+      {cartList.length === 0 && (
               <h1 className="no-items product">Le panier est vide</h1>
             )}
             {cartList.map((item) => (
@@ -115,9 +126,10 @@ const Cart = () => {
                 </Row>
               </div>
             ))}
-          </Col>
-          <Col md={4}>
-            <div className="cart-total">
+      </Item>
+
+      <Item>
+      <div className="cart-total">
               <h2>Résumé du panier</h2>
               <div className="d-flex">
                 <h4>Total :</h4>
@@ -133,8 +145,11 @@ const Cart = () => {
               Commander
             </Button>
             {message && <span>{message}</span>}
-          </Col>
-        </Row>
+      </Item>
+    </Grid>
+
+</Grid>
+       
       </Container>
     </section>
     </>
