e25464d23422dfa5eee80f34caf08ccc725eddc5
amirabourogaa <abouragba@u-label.com>
2024-03-02 00:33:32 +0100

    Change cart

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -73,7 +73,7 @@ const Cart = () => {
 
       <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
 
-    <Grid item xs={2} sm={4} md={12} >
+    <Grid item xs={8} sm={8} md={12} >
       <Item>
       {cartList.length === 0 && (
               <h1 className="no-items product">Le panier est vide</h1>
